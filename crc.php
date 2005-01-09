0d40d6619b48b27a6c0647ea4b4d515077927204
guillaume-util <guillaume-util@outters.eu>
2005-01-09 18:47:41 +0100

    récupération des fichiers Zip depuis l'alboretum
    + CRC.
    + FichierZip.
    
    darcs-hash:e627e0a5a140fbe48ac8658afd303ed729b6a5ca

---
+++ crc.php
@@ -0,0 +1,44 @@
+<?php
+
+/* Apparemment tout le monde a son implémentation incompatible. En voici donc
+ * intégralement recopiée sur le code de Marcel van Kervinck
+ * (<http://brick.bitpit.net/~marcelk/crc32>), code qui avait l'immense
+ * avantage de pouvoir être rapidement compilé et de donner le même résultat que
+ * celui de l'outil zip livré avec Darwin. Ensuite, il a l'air de prendre la
+ * voie "little endian", mais tant que ça marche chez moi… Et, joie, on découvre
+ * le même résultat qu'avec la fonction livrée. */
+
+function crc32_continu($donnees, $valeurCourante = 0)
+{
+	/* Si on peut passer par du code C, on ne va pas se gêner; en particulier,
+	 * le crc32 livré avec PHP se réinitialise à chaque appel, mais ça nous
+	 * permet au moins de l'appeler pour notre première itération. */
+	
+	if($valeurCourante == 0) return crc32($donnees);
+	
+	/* Initialisation */
+	
+	if(!array_key_exists('crc32_statique', $GLOBALS))
+	{
+		$polynome = 0xedb88320;
+		
+		for($octet = 0; $octet < 0x100; ++$octet)
+		{
+			for($reste = $octet ^ 0xff, $bit = 8; --$bit >= 0;)
+				$reste = (($reste >> 1) & 0x7fffffff) ^ (($reste & 0x1) ? $polynome : 0);
+			$s[$octet] = $reste ^ 0xff000000;
+		}
+		$GLOBALS['crc32_statique'] = $s;
+	}
+	else
+		$s = $GLOBALS['crc32_statique'];
+	
+	/* Calcul */
+	
+	for($n = strlen($donnees), $i = 0; $i < $n; ++$i)
+		$valeurCourante = $s[(ord($donnees{$i}) ^ $valeurCourante) & 0xff] ^ (($valeurCourante >> 8) & 0x00ffffff);
+	
+	return $valeurCourante;
+}
+
+?>
---
+++ fichierzip.php
@@ -0,0 +1,147 @@
+<?php
+/*
+ * Copyright (c) 2003-2004 Guillaume Outters
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+require_once('crc.php');
+
+/* Écriture de fichier Zip. La référence se trouve à
+ * <http://www.pkware.com/products/enterprise/white_papers/appnote.html>.
+ * Merci beaucoup:
+ * - à PKWare pour avoir eu la brillante idée de faire du little-endian
+ * - à l'équipe PHP pour avoir bien oublié de faire un CRC32 progressif */
+/* À FAIRE:
+ * - récupérer la date de modif des fichiers et en faire une date DOS pour les
+ *   dates de modif.
+ * - utiliser un algo de compression pour certains fichiers compressibles.
+ */
+
+class FichierZip
+{
+	public $infos = array();
+	public $methode = 1; // 0: stored; 1: deflated à la main; voir l'appnote.iz d'Info-ZIP pour savoir ce qui est bien ou non. Si, avec un zip généré en mode 0, unzip sous Mac OS X passe sans problème, on constate que zip -T est beaucoup moins prêt à laisser passer.
+	/* ATTENTION: PHP propose une méthode gzdeflate, qui donnerait le bon
+	 * résultat mais demande un retravail puisqu'elle marque chacun des blocs
+	 * qui lui est passé comme final. J'ai expérimenté en commençant par un
+	 * gzdeflate(…, 0) (qui doit donner la même chose que mon deflate à la
+	 * main), si je lui passe des blocs de 4K il prend le bloc et lui met le
+	 * marqueur, mais si je passe des blocs de 64K, il passe le bloc tel quel en
+	 * lui collant après coup un bloc vide avec le marqueur. */
+	
+	function ajouterFichier($nomFichierDansArchive, $cheminFichier)
+	{
+		if(!($f = @fopen($cheminFichier, 'r'))) return;
+		
+		/* En-tête */
+		
+		print($this->enTeteFichier($nomFichierDansArchive));
+		
+		/* Contenu */
+		
+		$taille = 0;
+		$controle = 0;
+		if($this->methode) $tailleCompr = 0;
+		while(!feof($f))
+		{
+			$octets = fread($f, 0x10000); // On ne va pas y aller à la petite cuillère.
+			$tailleBloc = strlen($octets);
+			$taille += $tailleBloc;
+			$controle = crc32_continu($octets, $controle);
+			if($this->methode)
+			{
+				$tailleCompr += $tailleBloc + 5;
+				print(pack('cvv', 0, $tailleBloc, -1 - $tailleBloc));
+			}
+			print($octets);
+		}
+		if($this->methode)
+		{
+			print(pack('cvv', 1, 0, -1)); // Un bloc vide pour terminer
+			$tailleCompr += 5;
+		}
+		
+		/* Résumé */
+		
+		if(!$this->methode) $tailleCompr = $taille;
+		print(pack('NVVV', 0x504b0708, $controle, $tailleCompr, $taille));
+		$this->infos[] = array($controle, $tailleCompr, $taille, 0, $nomFichierDansArchive); // On aura besoin de ressortir les infos sur le fichier à la fin.
+		
+		fclose($f);
+	}
+	
+	function clore()
+	{
+		/* Ponte du récapitulatif de fin d'archive */
+		
+		$positionCourante = 0;
+		$tailleTdM = 0;
+		foreach($this->infos as $infos)
+		{
+			$infos[3] = $positionCourante;
+			print($this->enTeteFichier($infos[4], $infos));
+			$positionCourante += 0x1e + strlen($infos[4]) + $infos[1] + 0x10; // Place prise par le fichier dans l'archive
+			$tailleTdM += 0x2e + strlen($infos[4]); // Taille prise par cette entrée dans la table des matières
+		}
+		
+		/* Clotûre globale */
+		
+		$nombre = count($this->infos);
+		print(pack('NvvvvVVv',
+			0x504b0506, // end of central dir signature
+			0, // number of this disk
+			0, //number of the disk with the start of the central directory
+			$nombre, // total number of entries in the central directory on this disk
+			$nombre, // total number of entries in the central
+			$tailleTdM, // size of the central directory
+			$positionCourante, // offset of start of central directory with respect to the starting disk number
+			0)); // .ZIP file comment length
+	}
+	
+	/* Génère un en-tête pour un fichier. Si $infos est renseigné (il doit
+	 * contenir le CRC-32, la taille prise dans l'archive, la taille du fichier
+	 * décompressé, et la position en octets du fichier dans le Zip), un en-tête
+	 * récapitulatif est généré (pour être utilisé dans la table des matières du
+	 * Zip). Sinon un simple en-tête est créé, qui devra être suivi des données. */
+	function enTeteFichier($nomFichier, $infos = null)
+	{
+		$r =
+			($infos !== null
+			? pack('Nv', 0x504b0102, 0x0000) // central file header signature, version made by
+			: pack('N', 0x504b0304)). // local file header signature
+			pack('vvvvv',
+				0x0014, // version needed to extract
+				0x0008, // general purpose bit flag
+				$this->methode ? 0x0008 : 0x0000, // compression method
+				0x0000, // last mod file time
+				0x0000). // last mod file date
+			($infos !== null
+			? pack('VVV', $infos[0], $infos[1], $infos[2]) // crc-32, compressed size, uncompressed size
+			: pack('VVV', 0, 0, 0)).
+			pack('vv', strlen($nomFichier), 0). // file name length, extra field length
+		($infos !== null
+		? pack('vvvVV', 0, 0, 0, 0, $infos[3]) // file comment length, disk number start, internal file attributes, external file attributes, relative offset of local header 
+		: '').
+			$nomFichier; // file name, extra field (vide), file comment (vide)
+		return $r;
+ 	}
+}
+
+?>
