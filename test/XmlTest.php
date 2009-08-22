ecfb0d20eede436e304724d7d8e5fdb51b0d48d8
guillaume-util <guillaume-util@outters.eu>
2009-08-22 17:12:34 +0200

    xml: test
    + Test CompoSimple.
    
    darcs-hash:ef0170c406fbad73048f58b0bba99f3861696fac

---
+++ test/XmlTest.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once dirname(__FILE__).'/../xml/composimple.php';
+require_once dirname(__FILE__).'/../xml/chargeur.php';
+
+class XmlTest extends PHPUnit_Framework_TestCase
+{
+	function testOnEcraseALaRelecture()
+	{
+		file_put_contents('/tmp/test.util', <<<TERMINE
+<?xml version="1.0"?>
+<racine>
+	<chose>coucou</chose>
+</racine>
+TERMINE
+);
+		$c = new Chargeur();
+		$o = new CompoBidon();
+		$c->charger('/tmp/test.util', 'racine', $o, true);
+		$c->charger('/tmp/test.util', 'racine', $o, true);
+		assert($o->chose == 'coucou');
+	}
+}
+
+class CompoBidon extends CompoSimple
+{
+	function CompoBidon()
+	{
+		$this->CompoSimple(array('chose' => &$this->chose));
+	}
+}
+
+?>
