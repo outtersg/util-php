807a9fbbc25385b85e6d1f7acc3a4e83462fd05a
guillaume-util <guillaume-util@outters.eu>
2009-09-24 02:23:47 +0200

    params: simplification des tableaux passés en paramètre GET
    = Si possible, on utilise une notation machin[]=&machin[]=, plutôt que de mentionner à chaque fois les indices. PHPMyAdmin a horreur des tableaux lui forçant les index.
    
    darcs-hash:ced9f6e36132bce204a0b8997f5c1e89648fecb7

--- test/ParamsTest.php
+++ test/ParamsTest.php
@@ -4,6 +4,12 @@ require_once dirname(__FILE__).'/../params.inc';
 
 class ParamsTest extends PHPUnit_Framework_TestCase
 {
+	function testSimplificationDesTableaux()
+	{
+		$this->assertEquals('&chose[]=truc&chose[]=machin', params_decomposer(null, array('chose' => array(0 => 'truc', 1 => 'machin'))));
+		$this->assertEquals('&chose[1]=truc&chose[2]=machin', params_decomposer(null, array('chose' => array(1 => 'truc', 2 => 'machin'))));
+		$this->assertEquals('&chose[1]=truc&chose[0]=machin', params_decomposer(null, array('chose' => array(1 => 'truc', 0 => 'machin'))));
+	}
 }
 
 ?>
