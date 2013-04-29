fcbcf72ec7f0d1ad80ef97918e499152b46742f6
guillaume-util <guillaume-util@outters.eu>
2013-04-29 22:55:09 +0200

    test: Date::comparer() flou
    = Un peu de tests ne fait pas de mal. Surtout quand les tests passent (youpi!).
    
    darcs-hash:27f680b470300bd24a70dc1ee096e4a4476fb313

--- test/PeriodeTest.php
+++ test/PeriodeTest.php
@@ -21,6 +21,22 @@ class PeriodeTest extends PHPUnit_Framework_TestCase
 			$this->assertEquals($ensemble[3], Periode::aff($ensemble[0], $ensemble[1], Periode::$REDIGE));
 		}
 	}
+	
+	function testComparer()
+	{
+		$d0 = array(2012, 06, 22, 15, 26, 13);
+		$d1 = array(2012, 06, 22, null, null, null);
+		
+		$this->assertEquals(0, Date::comparer($d0, $d1)); // Sans plus de précision, on élide les HMS de la première date puisque la seconde n'offre aucun point de comparaison: les parties renseignées de part et d'autre concordent, on estime donc l'égalité.
+		$this->assertGreaterThan(0, Date::comparer($d0, $d1, false)); // Par défaut, il est 0 h 00 si on n'a pas de précision.
+		$this->assertLessThan(0, Date::comparer($d0, $d1, false, 0.25)); // … sauf si la date indéfinie ne couvre que le dernier quart de sa journée (de 18 à 24 h).
+		$this->assertLessThan(0, Date::comparer($d0, $d1, true)); // Si la seconde date est une fin (à date incluse), elle s'étend jusqu'à la fin de la plage (ici la journée).
+		$this->assertGreaterThan(0, Date::comparer($d0, $d1, true, 0.25)); // Bon bien sûr si la « fin de journée » n'en couvre qu'un quart, elle s'arrête à 8 h du mat', c'est-à-dire avant la date fixe.
+		$this->assertEquals(0, Date::comparer($d1, $d0)); // Et dans l'autre sens (la date indéfinie passe en tant que début)?
+		$this->assertLessThan(0, Date::comparer($d1, $d0, false));
+		$this->assertLessThan(0, Date::comparer($d1, $d0, true));
+		$this->assertGreaterThan(0, Date::comparer($d1, $d0, false, 0.25));
+	}
 }
 
 ?>
